fb52aa1ff4b9aaa88f1cbbcad14984e8a12b54cc
MatteoGuerra17 <73823902+MatteoGuerra17@users.noreply.github.com>
2021-08-18 12:03:06 +0200

    add difficulty button

--- StartMenu.java
+++ StartMenu.java
@@ -9,25 +9,31 @@ public class StartMenu extends MouseAdapter{
 	private int xMouse; 
 	private int yMouse;
 	private int xPlayBtn, yPlayBtn, widthPlayBtn, heightPlayBtn;
-	private int xMenuBtn, yMenuBtn, widthMenuBtn, heightMenuBtn;
+	private int xTutorialBtn, yTutorialBtn, widthTutorialBtn, heightTutorialBtn;
 	private int xQuitBtn, yQuitBtn, widthQuitBtn, heightQuitBtn;
+	private int xDiffBtn, yDiffBtn, widthDiffBtn, heightDiffBtn;
 	
 	public StartMenu(Game game) {
 		this.game = game;
 		this.xPlayBtn = game.getWidth()/3;
 		this.yPlayBtn = game.getHeight()/4;
 		this.widthPlayBtn = game.getWidth()/3;
-		this.heightPlayBtn = game.getHeight()/6;
+		this.heightPlayBtn = game.getHeight()/8;
 		
-		this.xMenuBtn = game.getWidth()/3;
-		this.yMenuBtn = game.getHeight()/4 + game.getHeight()/4;
-		this.widthMenuBtn = game.getWidth()/3;
-		this.heightMenuBtn = game.getHeight()/6;
+		this.xDiffBtn = game.getWidth()/3;
+		this.yDiffBtn = game.getHeight()/128 + 68*(game.getHeight()/128);
+		this.widthDiffBtn = game.getWidth()/3;
+		this.heightDiffBtn = game.getHeight()/8;
+		
+		this.xTutorialBtn = game.getWidth()/3;
+		this.yTutorialBtn = game.getHeight()/128 + 96*(game.getHeight()/128);
+		this.widthTutorialBtn = game.getWidth()/3;
+		this.heightTutorialBtn = game.getHeight()/8;
 		
 		this.xQuitBtn = game.getWidth()/3;
 		this.yQuitBtn = game.getHeight()/4 + 2*(game.getHeight()/4);
 		this.widthQuitBtn = game.getWidth()/3;
-		this.heightQuitBtn = game.getHeight()/6;
+		this.heightQuitBtn = game.getHeight()/8;
 	}
 
 	public void mousePressed(MouseEvent e) {
@@ -60,23 +66,27 @@ public class StartMenu extends MouseAdapter{
 	}
 	
 	public void render(Graphics g) {
-		Font font = new Font("Times New Roman", 1, 100);
-		Font font2 = new Font("arial", 1, 50);
+		Font font = new Font("Times New Roman", 1, 75);
+		Font font2 = new Font("arial", 1, 25);
 		
 		g.setColor(Color.red);
 		g.setFont(font);
-		g.drawString("Menu", game.getWidth()/128 + 49*(game.getWidth()/128), game.getHeight()/128 + 30*(game.getHeight()/128));
+		g.drawString("Menu", game.getWidth()/128 + 53*(game.getWidth()/128), game.getHeight()/128 + 30*(game.getHeight()/128));
 		
 		g.setColor(Color.white);
 		g.setFont(font2);
 		g.drawRect(this.xPlayBtn, this.yPlayBtn, this.widthPlayBtn, this.heightPlayBtn);
-		g.drawString("Play", game.getWidth()/128 + 59*(game.getWidth()/128), game.getHeight()/128 + 60*(game.getHeight()/128));
+		g.drawString("Play", game.getWidth()/128 + 62*(game.getWidth()/128), game.getHeight()/128 + 54*(game.getHeight()/128));
 		
-		g.drawRect(this.xMenuBtn, this.yMenuBtn, this.widthMenuBtn, this.heightMenuBtn);
-		g.drawString("Tutorial", game.getWidth()/128 + 53*(game.getWidth()/128), game.getHeight()/128 + 102*(game.getHeight()/128));
+		g.drawRect(this.xDiffBtn, this.yDiffBtn, this.widthDiffBtn, this.heightDiffBtn);
+		g.drawString("Difficulty", game.getWidth()/128 + 58*(game.getWidth()/128), game.getHeight()/128 + 81*(game.getHeight()/128));
+		
+		g.drawRect(this.xTutorialBtn, this.yTutorialBtn, this.widthTutorialBtn, this.heightTutorialBtn);
+		g.drawString("Tutorial", game.getWidth()/128 + 59*(game.getWidth()/128), game.getHeight()/128 + 109*(game.getHeight()/128));
 		
 		g.drawRect(this.xQuitBtn, this.yQuitBtn, this.widthQuitBtn, this.heightQuitBtn);
-		g.drawString("Quit", game.getWidth()/128 + 59*(game.getWidth()/128), game.getHeight()/128 + 142*(game.getHeight()/128));
+		g.drawString("Quit", game.getWidth()/128 + 62*(game.getWidth()/128), game.getHeight()/128 + 137*(game.getHeight()/128));
+		
 	}
 	
 }
